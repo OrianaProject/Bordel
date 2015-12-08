0aa6b5dd85002aa3b45063941a22d1d09bec932d
The Great Prophet <postumus.mkernh@gmail.com>
2015-12-08 14:03:48 +0100

    Update ft_strsub.c

--- libft/ft_strsub.c
+++ libft/ft_strsub.c
@@ -19,7 +19,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	int		i;
 
 	i = 0;
-	if (!(copie = (char*)malloc(sizeof(char) * (len - start + 1))))
+	if (!(copie = (char*)malloc(sizeof(char) * (len + 1))))
 		return (0);
 	while (i < (int)len)
 	{
